e4dc7c65c2ca6827a42cef8f095dcc178975af62
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-02-07 20:41:46 +0530

    :sparkles: completed testing stripe payments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import "./styles.css";
 import { CardElement, injectStripe, Elements } from "react-stripe-elements";
 
 function _CardForm(props) {
-  const handleSubmit = async e => {
+  let [error, setError] = useState<string>("");
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-
+    error && setError("");
     try {
       let clientSecret: any = await fetch("http://localhost:8080/secret-key", {
         method: "GET",
@@ -21,14 +22,18 @@ function _CardForm(props) {
             card: props.elements.getElement("card"),
             billing_details: {
               name: "Jenny Rosen",
-              address: "123 State St Schenectady, NY 12345"
+              address: "10901 W. 120th Ave, Suite 235 Broomfield, CO 80021"
             }
           }
         }
       );
-      console.log("confirmation:", confirmation);
+      if (confirmation.error)
+        setError(
+          confirmation.error.message || "Something's wrong I can feel it"
+        );
     } catch (error) {
       console.error(error);
+      setError(error.message || "Something's wrong I can feel it");
     }
   };
 
@@ -39,6 +44,8 @@ function _CardForm(props) {
         <CardElement />
       </label>
       <input type="submit" value="Complete Payment" />
+      <br />
+      <span style={{ color: "red" }}>{error}</span>
     </form>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
+import dotenv from "dotenv";
 import React from "react";
 import ReactDOM from "react-dom";
 import { StripeProvider } from "react-stripe-elements";
-
 import App from "./App";
 
-console.log(process.env.REACT_APP_STRIPE_KEY);
+dotenv.config();
+
+console.log(process.env.REACT_APP_SECRET_KEY);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   // @ts-ignore
-  <StripeProvider apiKey={process.env.REACT_APP_STRIPE_KEY}>
+  <StripeProvider apiKey={process.env.REACT_APP_SECRET_KEY}>
     <App />
   </StripeProvider>,
   rootElement
